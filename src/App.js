56b2fbc032c6c7f6b4aa060d7f2a9689d5fe102e
Rohan <rarihant@falabella.cl>
2020-05-10 14:07:00 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          new to react
         </a>
       </header>
     </div>
